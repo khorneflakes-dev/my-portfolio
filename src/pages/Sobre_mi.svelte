dd8a247a0ada0d9bd2a57d2f3b020a10643777b6
khorne flakes <khorneflakesdev@gmail.com>
2022-12-21 19:43:04 -0400

    a

--- src/pages/Sobre_mi.svelte
+++ src/pages/Sobre_mi.svelte
@@ -7,83 +7,84 @@
             Me inicie en el mundo de la programación de manera autodidacta, cuando escribí mi primer script de web scraping para obtener precios de una tienda virtual de licores.
             Motivado por lo que podría llegar a hacer con Python, encontré el bootcamp de Henry.
             Desde ahí he adquirido conocimientos en Python, SQL, GCP y herramientas de visualización de datos como Dash, Plotly, Tableau y Looker Studio.
-            <br>        
+            <br>
             Me interesa seguir desarrollando mis habilidades y adquiriendo nuevos conocimientos en este campo dinámico y emocionante, y estoy dispuesto a trabajar duro para lograr mis metas en el mundo de los datos.
         </div>
     </div>
 
+    <div class="title-exp">Experiencia</div>
     <div class="experience">
-        <div class="title-exp">Experiencia</div>
-        <div class="exp">
-            <div class="cargo">Analista de datos [académico]</div>
-            <div class="empresa">Henry</div>
-            <div class="tiempo">2022(nov) a 2022(dic), Argentina</div>
-            <div class="exp-description">
-                Como colaborador en un proyecto de análisis de datos, mis tareas principales fueron la creación y administración de pipelines de ETL (extracción, transformación y carga de datos) en datasets de gran tamaño. Además, construí y administré la base de datos en Google Cloud SQL que se usó en el proyecto.
-                También realicé un análisis de las tendencias de consumo de los usuarios para construir un sistema de recomendación personalizado. Finalmente, utilicé las herramientas Dash y Plotly para desarrollar una aplicación web donde se mostraron los dos productos finales del proyecto.
-            </div>
-        </div>
-        <div class="exp">
-            <div class="cargo">Analista de datos [académico]</div>
-            <div class="empresa">Henry</div>
-            <div class="tiempo">2022(oct) a 2022(nov), Argentina</div>
-            <div class="exp-description">
-                Realicé trabajos de ETL y análisis sobre datasets de accidentes aéreos obtenidos de la Organización de Aviación Civil Internacional.
-                Construí un dashboard que se puede visualizar en la web utilizando Dash y Plotly, con el fin de presentar los resultados del análisis.
-            </div>
-        </div>
+        <!-- <div class="line-container">
+            <div class="lines"></div>
+        </div> -->
+        <div class="experience-container">
 
-        <div class="exp">
-            <div class="cargo">Analista de datos [académico]</div>
-            <div class="empresa">Maven Analytics</div>
-            <div class="tiempo">2022(sept) a 2022(oct)</div>
-            <div class="exp-description">
-                Desarrollé un dashboard en formato de aplicación web utilizando Dash y Plotly en base al analisis de varios datasets con informacion referente a las operaciones de una pizzeria del año 2015.
-                El caso de estudio planteado por Maven Analytics se centró en la optimización del proceso de trabajo y atención a los clientes en una pizzería.
-                Link de los requerimientos minimos del caso de estudio: 
-                <a href="https://www.mavenanalytics.io/blog/maven-pizza-challenge">Maven Pizza Challenge</a>
-                
+            <div class="exp">
+                <div class="cargo">Analista de datos en Henry (académico)</div>
+                <div class="tiempo">nov. 2022 a dic. 2022, Argentina</div>
+                <div class="exp-description">
+                    Como colaborador en un proyecto de análisis de datos, mis tareas principales fueron la creación y administración de pipelines de ETL (extracción, transformación y carga de datos) en datasets de gran tamaño. Además, construí y administré la base de datos en Google Cloud SQL que se usó en el proyecto.
+                    También realicé un análisis de las tendencias de consumo de los usuarios para construir un sistema de recomendación personalizado. Finalmente, utilicé las herramientas Dash y Plotly para desarrollar una aplicación web donde se mostraron los dos productos finales del proyecto.
+                </div>
             </div>
-        </div>
-
-        <div class="exp">
-            <div class="cargo">Analista Comerial</div>
-            <div class="empresa">Inox Supply Ingenieria</div>
-            <div class="tiempo">2020 a 2022, La Paz, Bolivia</div>
-            <div class="exp-description">
-                Uso de herramientas de análisis de datos para identificar tendencias y patrones en el comportamiento de los clientes y el inventario.
-                Elaboración de informes de análisis de datos de clientes e inventario usando tablas dinamicas y graficas.
-                Comunicación con el equipo de marketing para coordinar la implementación de los planes de acción.
+            <div class="exp">
+                <div class="cargo">Analista de datos en Henry (académico)</div>
+                <div class="tiempo">oct. 2022 a nov. 2022, Argentina</div>
+                <div class="exp-description">
+                    Realicé trabajos de ETL y análisis sobre datasets de accidentes aéreos obtenidos de la Organización de Aviación Civil Internacional.
+                    Construí un dashboard que se puede visualizar en la web utilizando Dash y Plotly, con el fin de presentar los resultados del análisis.
+                </div>
             </div>
-        </div>
-
-        <div class="exp">
-            <div class="cargo">Auxiliar de Auditoria</div>
-            <div class="empresa">CMLP - Comisión de Desarrollo Económico y Financiero</div>
-            <div class="tiempo">2018 a 2020, La Paz, Bolivia</div>
-            <div class="exp-description">
-                Trabajo en el área de Control de Obras del municipio de La Paz en colaboración con el equipo de abogados y arquitectos, donde se lleva a cabo el seguimiento, análisis y evaluación de los contratos de obras públicas.
-                Medición del cumplimiento de los plazos y presupuestos establecidos en los contratos, y del grado de satisfacción de la comunidad con las obras realizadas
+    
+            <div class="exp">
+                <div class="cargo">Analista de datos en Maven Analytics (académico)</div>
+                <div class="tiempo">sept. 2022 a oct. 2022</div>
+                <div class="exp-description">
+                    Desarrollé un dashboard en formato de aplicación web utilizando Dash y Plotly en base al analisis de varios datasets con informacion referente a las operaciones de una pizzeria del año 2015.
+                    El caso de estudio planteado por Maven Analytics se centró en la optimización del proceso de trabajo y atención a los clientes en una pizzería.
+                    Link de los requerimientos minimos del caso de estudio: 
+                    <a href="https://www.mavenanalytics.io/blog/maven-pizza-challenge">Maven Pizza Challenge</a>
+                    
+                </div>
+            </div>
+    
+            <div class="exp">
+                <div class="cargo">Analista Comerial en Inox Supply Ingenieria</div>
+                <div class="tiempo">2020 a 2022, La Paz, Bolivia</div>
+                <div class="exp-description">
+                    Uso de herramientas de análisis de datos para identificar tendencias y patrones en el comportamiento de los clientes y el inventario.
+                    Elaboración de informes de análisis de datos de clientes e inventario usando tablas dinamicas y graficas.
+                    Comunicación con el equipo de marketing para coordinar la implementación de los planes de acción.
+                </div>
+            </div>
+    
+            <div class="exp">
+                <div class="cargo">Auxiliar de Auditoria en CMLP - Comisión de Desarrollo Económico y Financiero</div>
+                <div class="tiempo">2018 a 2020, La Paz, Bolivia</div>
+                <div class="exp-description">
+                    Trabajo en el área de Control de Obras del municipio de La Paz en colaboración con el equipo de abogados y arquitectos, donde se lleva a cabo el seguimiento, análisis y evaluación de los contratos de obras públicas.
+                    Medición del cumplimiento de los plazos y presupuestos establecidos en los contratos, y del grado de satisfacción de la comunidad con las obras realizadas
+                </div>
             </div>
         </div>
         
-        <div class="education">
-            <div class="title-education">Formacion</div>
-            <div class="ed">
-                <div class="ed-title">Data Scientist</div>
-                <div class="description">2022 a 2022 en Henry Bootcamp, Argentina</div>
-            </div>
-    
-            <div class="ed">
-                <div class="ed-title">Auditoria Contable (ultimo año)</div>
-                <div class="description">2020 a 2022 en Universidad NUR, Bolivia</div>
-            </div>
-    
-            <div class="ed">
-                <div class="ed-title">Contabilidad General (Tecnicatura)</div>
-                <div class="description">2015 a 2018 en Instituto de Educacion Bancaria, Bolivia</div>
-            </div>
-    
+        
+    </div>
+    <div class="education">
+        <div class="title-education">Formacion</div>
+        <div class="ed">
+            <div class="ed-title">Data Scientist</div>
+            <div class="description">2022 a 2022 en Henry Bootcamp, Argentina</div>
+        </div>
+
+        <div class="ed">
+            <div class="ed-title">Auditoria Contable (ultimo año)</div>
+            <div class="description">2020 a 2022 en Universidad NUR, Bolivia</div>
+        </div>
+
+        <div class="ed">
+            <div class="ed-title">Contabilidad General (Tecnicatura)</div>
+            <div class="description">2015 a 2018 en Instituto de Educacion Bancaria, Bolivia</div>
         </div>
 
     </div>
@@ -93,11 +94,68 @@
     .perfil{
         margin-left: 35vw;
         margin-right: 18vw;
-        font-family: 'Roboto', sans-serif;
+        font-family: var(--fuente-principal);
         color: var(--color-texto);
         padding-top: 10vh;
         display: flex;
         flex-direction: column;
         gap: 1vh;
+        color: var(--color-texto);
     }
+    .header{
+        display: flex;
+        flex-direction: column;
+        font-family: var(--fuente-principal);
+        gap: 1rem;
+    }
+    .title, .title-exp{
+        font-size: 2rem;
+        font-weight: 600;
+    }
+    .header .description{
+        font-size: 1rem;
+        padding-left: 0.3rem;
+    }
+    .experience{
+        display: flex;
+        gap: 2rem;
+    }
+    .experience-container{
+        display: flex;
+        flex-direction: column;
+        padding-left: 0.5rem;
+        gap: 1.5rem;
+    }
+    .exp{
+        display: flex;
+        flex-direction: column;
+        gap: 0.3rem;
+    }
+    .cargo{
+        font-size: 1rem;
+        font-weight: 500;
+    }
+    .tiempo{
+        width: 100;
+        font-weight: 400;
+        filter: opacity(90%);
+    }
+    .exp-description{
+        font-weight: 400;
+    }
+    a{
+        text-decoration: none;
+    }
+    /* .line-container{
+        width: 0.1rem;
+        display: flex;
+        flex-direction: column;
+    }
+    .lines{
+        width: 0.7rem;
+        height: 0.7rem;
+        border-radius: 50%;
+        background: var(--color-texto);
+    } */
+    
 </style>
